f6386bf9e56f8fd63406813ea52d015f8376ce97
dheeraj sharma <dheeraj.sharma@dataclaps.com>
2024-08-06 19:47:50 +0530

    Created apps and APIs.

--- ORM/settings.py
+++ ORM/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'daphne',
+    # 'daphne',
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
