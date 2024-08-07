806e27c20730aa49865ad72b5a1209e79cfcee95
dheeraj sharma <dheeraj.sharma@dataclaps.com>
2024-08-07 11:27:06 +0530

    removed migrations and .pyc files.

--- apps/pagi_college/models.py
+++ apps/pagi_college/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Department(models.Model):
+class Department1(models.Model):
     name = models.CharField(max_length=100)
 
 
 class Subject(models.Model):
     name = models.CharField(max_length=100)
-    departments = models.ManyToManyField(Department, null=True, blank=True)
+    departments = models.ManyToManyField(Department1, null=True, blank=True)
