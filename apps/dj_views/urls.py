3d539bf887639f080c621b6323826b30f4459904
dheeraj sharma <dheeraj.sharma@dataclaps.com>
2024-08-24 20:44:33 +0530

    Tested the redirection of requests.

--- apps/dj_views/urls.py
+++ apps/dj_views/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('get-states/', StateList.as_view(), name='get-states-list'),
     path('get-single-state/<str:name>/', StateSingle.as_view()),
     path('g-state/', StateGenericAPIView.as_view(), name='state-generic-api-view'),
+    path('method/', get_state, name='method-get-states'),
+    path('country/', get_countries, name = 'method-get-countries')
 ]
 
 urlpatterns += router.urls
--- apps/dj_views/views.py
+++ apps/dj_views/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import get_object_or_404, render
 from rest_framework.response import Response
+from django.http import JsonResponse, HttpResponse
+from rest_framework.decorators import api_view
+from django.views.decorators.http import require_http_methods, require_POST
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import action
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import HttpResponseRedirect, redirect, reverse
 
 from rest_framework.generics import GenericAPIView, ListAPIView
 from rest_framework import generics
@@ -11,6 +18,32 @@ from .serializers import *
 from .views import *
 from .filters import *
 import django_filters
+
+# @login_required
+# @api_view(['GET', 'POST'])
+def get_state(request, **kwargs):
+    if request.method == 'GET':
+        queryset = State.objects.all()
+        data = StateSerializer(queryset, many=True).data
+        return JsonResponse(data, status=200, safe=False)
+    else:
+        return JsonResponse({"error": "Invalid method type, Please use get method only"}, status=400)
+
+# @require_http_methods(['PUT'])
+@csrf_exempt
+def get_countries(request, **kwargs):
+    if request.method == "GET":
+        queryset = Country.objects.all()
+        data = CountrySerializer(queryset, many=True).data
+        return JsonResponse(data=data, status=200, safe=False)
+    else:
+        print("get_countries method is not GET")
+        response = HttpResponseRedirect('/views/g-state/')
+        print("type of httpresponseredirect: ", type(response), 'data: ', response.status_code)
+
+        response = redirect('/views/g-state/')
+        print("type of redirect: ", type(response), 'data: ', type(response.content))
+        return response
 
 
 class StateList(ListAPIView):
@@ -61,4 +94,10 @@ class StateGenericViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, view
         if self.request.method == 'PUT':
             queryset = queryset.filter(name__icontains='india')
         return queryset
-
+    
+    @action(methods=['GET'], detail=True)
+    @login_required
+    def get_state(self, request, pk=None, **kwargs):
+        obj = self.get_object()
+        data = StateSerializer(obj).data
+        return Response(data, status=200)
